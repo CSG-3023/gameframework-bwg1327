8a74ab354bb4240b0cd62140b3a56839c78e762d
bwg1327 <bwg1327@utulsa.edu>
2022-07-20 19:29:45 -0500

    Game Manager p6
    menu_scene is named main_menu in my version.
    When running acutal title screen says made by me instead of my name, need to look through old instructions if this is allowed.

--- ProjectTemp-Unity/Assets/Scripts/MenuScript.cs
+++ ProjectTemp-Unity/Assets/Scripts/MenuScript.cs
@@ -3,7 +3,7 @@
 * Date Created: July 5, 2022
 *
 * Last Edited by: Brennan Gillespie
-* Last Edited:
+* Last Edited: July 20, 2022
 *
 * Description: Manages the UI elements for the menu scenes
 ****/
@@ -17,6 +17,8 @@ using TMPro; //libraries for TextMeshPro components
 
 public class MenuScript : MonoBehaviour
 {
+    GameManager gm; //reference to game manager
+
     [Header("Text Boxes")]
     public TMP_Text titleTextbox; //textbox for the title
     public TMP_Text creditsTextbox; //textbox for the credits
@@ -26,17 +28,26 @@ public class MenuScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //set the value for textboxes
+        gm = GameManager.GM; //refernce to game manager
 
-    }//end Start()
+        //If text boxes exist set the value
+        if (titleTextbox) { titleTextbox.text = gm.gameTitle; }
+        if (creditsTextbox) { creditsTextbox.text = gm.gameCredits; }
+        if (copyrightTextbox) { copyrightTextbox.text = gm.copyrightDate; }
+        if (mesageTextbox) { mesageTextbox.text = gm.endMsg; }
+   }//end Start()
 
     public void OnGameStart()
     {
         Debug.Log("Game Started");
+        gm.SetTargetState(GameState.gamePlaying);
+
     }//end OnGameStart()
 
     public void OnGameExit()
     {
         Debug.Log("Exited Game");
+        gm.SetTargetState(GameState.gameExited);
+
     }//end OnGameExit()
 }
